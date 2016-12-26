770f9370f640e504f93dd1ed660dad9ef1854b6f
bdanglot <bdanglot@gmail.com>
2016-12-26 10:50:29 +0100

    wip: spoon 5.5.0

--- profiling/src/main/java/fr/inria/diversify/processor/main/AbstractLoggingInstrumenter.java
+++ profiling/src/main/java/fr/inria/diversify/processor/main/AbstractLoggingInstrumenter.java
@@ -9,6 +9,7 @@ import spoon.reflect.factory.Factory;
 import spoon.reflect.visitor.Query;
 import spoon.reflect.visitor.QueryVisitor;
 import spoon.reflect.visitor.filter.TypeFilter;
+import spoon.support.reflect.code.CtTryImpl;
 
 import java.util.HashMap;
 import java.util.HashSet;
@@ -124,7 +125,7 @@ public abstract class AbstractLoggingInstrumenter<E extends CtElement> extends A
             Factory factory = method.getFactory();
             CtStatement thisStatement = getThisOrSuperCall(method.getBody());
 
-            CtTry ctTry = factory.Core().createTry();
+            CtTryImpl ctTry = (CtTryImpl) factory.Core().createTry();
             ctTry.setBody(method.getBody());
 
             CtBlock finalizerBlock = factory.Core().createBlock();
